ba68866c546fa04b7737672c01d3d2420a0da6c9
olga84nyagina <93904024+olga84nyagina@users.noreply.github.com>
2021-11-14 17:03:38 +0300

    Update Main.java

--- task_2.1/Main.java
+++ task_2.1/Main.java
@@ -9,16 +9,17 @@
 •	Текст в каждой строке должен быть уникален.
 •	Слова в строке не должны повторяться т.е. (мамамамамама, мамамамамыла) итд не должно быть
 •	Должны быть выведены все возможные комбинации. */
-
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[]   args) {
         String[] str = { "мама", "мыла", "раму"};
         for (int a = 0; a < str.length; a = a + 1){
-    for (int b = 0; b < str.length; b = b + 1){
-        for (int c = 0; с < str.length; с = с + 1){
-
+        for (int b = 0; b < str.length; b = b + 1){
+        for (int c = 0; c < str.length; c = c + 1){
     System.out.println(str[a]+str[b]+str[c]);
         }
-    }
         }
+        }
+    }
+    }
+        
